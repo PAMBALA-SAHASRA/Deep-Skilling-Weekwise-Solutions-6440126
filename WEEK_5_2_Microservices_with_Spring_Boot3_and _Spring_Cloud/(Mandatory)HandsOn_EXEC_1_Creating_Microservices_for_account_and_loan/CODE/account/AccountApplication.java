5e55dbbd6f98427a474f0dc42923809b1847ac0e
PambalaSahasra <sahasrap620@gmail.com>
2025-07-20 17:32:31 +0530

    WEEK-5 HandsOn Exercises Completed

---
+++ WEEK_5_2_Microservices_with_Spring_Boot3_and _Spring_Cloud/(Mandatory)HandsOn_EXEC_1_Creating_Microservices_for_account_and_loan/CODE/account/AccountApplication.java
@@ -0,0 +1,14 @@
+package com.cognizant.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountApplication.class, args);
+	}
+
+}
+//D:\Users\Sahasra\OneDrive\Desktop\WEEK 5\123456\microservices\account
